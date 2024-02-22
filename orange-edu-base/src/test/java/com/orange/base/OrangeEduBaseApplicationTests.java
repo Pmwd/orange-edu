77c709eb2a454576b8cdf693b06c45c0b2905488
Pmwd <1454391528@qq.com>
2024-02-22 17:36:24 +0800

    add two mudules

---
+++ orange-edu-base/src/test/java/com/orange/base/OrangeEduBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeEduBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
