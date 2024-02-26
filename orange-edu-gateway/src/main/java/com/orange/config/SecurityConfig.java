44353ccf18e065a5f1da5ffc7fce673065ac2c13
Pmwd <1454391528@qq.com>
2024-02-27 00:08:28 +0800

    add gateway service

---
+++ orange-edu-gateway/src/main/java/com/orange/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.orange.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * 安全配置类
+ *
+ * @author Wuxy
+ * @version 1.0
+ * @since 2022/9/27 12:07
+ */
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    /**
+     *
+     * @param http
+     * @return
+     */
+    // 安全拦截配置
+    @Bean
+    public SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) {
+
+        return http.authorizeExchange()
+                .pathMatchers("/**").permitAll()
+                .anyExchange().authenticated()
+                .and().csrf().disable().build();
+    }
+
+}
